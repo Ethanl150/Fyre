10cb8bab4c742b2927430b29a393549a0d9dffb6
trion bailey <trionbailey@gmail.com>
2020-03-19 17:25:42 -0400

    orm

--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,9 @@
 DROP DATABASE IF EXISTS fyre_db;
+
 CREATE DATABASE fyre_db;
+
+USE fyre_db;
+
+CREATE TABLE fyreList(
+
+);
--- public/js/index.js
+++ public/js/index.js
@@ -1,99 +1,39 @@
-// Get references to page elements
-var $exampleText = $("#example-text");
-var $exampleDescription = $("#example-description");
-var $submitBtn = $("#submit");
-var $exampleList = $("#example-list");
+// var request=require("request");
+// let user_id=""
+// let token="Bearer"
+// var genre_url="https://api.spotify.com/v1/recommendations/available-genre-seeds" 
+var app = {} 
+console.log("world")
+// Allow user to choose genre
+app.getGenres = (genre) => $.ajax({
+    url: "https://api.spotify.com/v1/recommendations/available-genre-seeds",
+    method: "GET",
+    dataType: 'json',
+    data: {
+        type:"genre"
 
-// The API object contains methods for each kind of request we'll make
-var API = {
-  saveExample: function(example) {
-    return $.ajax({
-      headers: {
-        "Content-Type": "application/json"
-      },
-      type: "POST",
-      url: "api/examples",
-      data: JSON.stringify(example)
-    });
-  },
-  getExamples: function() {
-    return $.ajax({
-      url: "api/examples",
-      type: "GET"
-    });
-  },
-  deleteExample: function(id) {
-    return $.ajax({
-      url: "api/examples/" + id,
-      type: "DELETE"
-    });
-  }
-};
+    }
 
-// refreshExamples gets new examples from the db and repopulates the list
-var refreshExamples = function() {
-  API.getExamples().then(function(data) {
-    var $examples = data.map(function(example) {
-      var $a = $("<a>")
-        .text(example.text)
-        .attr("href", "/example/" + example.id);
+})
+//Go to spotify and get the genre
 
-      var $li = $("<li>")
-        .attr({
-          class: "list-group-item",
-          "data-id": example.id
-        })
-        .append($a);
+//display genre onto the page
+app.events = function () {
+    console.log("hello")
+    $('.list-group-item').on('click', function (event) {
+        event.preventDefault();
+        console.log("click")
+        console.log(this.innerText)
+        //let genres
+        app.getGenres(this.innerText)
+    })
+}
+app.events()
 
-      var $button = $("<button>")
-        .addClass("btn btn-danger float-right delete")
-        .text("ｘ");
-
-      $li.append($button);
-
-      return $li;
-    });
-
-    $exampleList.empty();
-    $exampleList.append($examples);
-  });
-};
-
-// handleFormSubmit is called whenever we submit a new example
-// Save the new example to the db and refresh the list
-var handleFormSubmit = function(event) {
-  event.preventDefault();
-
-  var example = {
-    text: $exampleText.val().trim(),
-    description: $exampleDescription.val().trim()
-  };
-
-  if (!(example.text && example.description)) {
-    alert("You must enter an example text and description!");
-    return;
-  }
-
-  API.saveExample(example).then(function() {
-    refreshExamples();
-  });
-
-  $exampleText.val("");
-  $exampleDescription.val("");
-};
-
-// handleDeleteBtnClick is called when an example's delete button is clicked
-// Remove the example from the db and refresh the list
-var handleDeleteBtnClick = function() {
-  var idToDelete = $(this)
-    .parent()
-    .attr("data-id");
-
-  API.deleteExample(idToDelete).then(function() {
-    refreshExamples();
-  });
-};
-
-// Add event listeners to the submit and delete buttons
-$submitBtn.on("click", handleFormSubmit);
-$exampleList.on("click", ".delete", handleDeleteBtnClick);
+// function getRandomTracks(num,tracks){
+//     var randomResults=[];
+//     for(let i = o; i<num;i++){
+//         randomResults.push(tracks[Math.floor(Math.random()* tracks.length)])
+//     }
+//     return randomResults;
+// }
