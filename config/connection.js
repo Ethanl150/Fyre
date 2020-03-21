66c4af35d3a8094d0e7423d9afb9e6f6b477c52a
Ethan Lacy <ethanr0m818@gmail.com>
2020-03-21 19:43:28 -0400

    added jaws_db

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,33 @@
 var Sequelize = require("sequelize");
 require("dotenv").config();
 
-var sequelize = new Sequelize("fyre_db", "root", process.env.MYSQL_PASS, {
-  host: "localhost",
-  port: 3306,
-  dialect: "mysql",
-  pool: {
-    max: 5,
-    min: 0,
-    idle: 10000
-  }
-});
+// if (process.env.JAWSDB_URL) {
+//   connection = mysql.createConnection(process.env.JAWSDB_URL)
+// } else {
+//   connection = mysql.createConnection({
+//     host: "localhost",
+//     port: 3306,
+//     user: "root",
+//     password: process.env.MySQL_PASS,
+//     database: "burgers_db"
+//   });
+// }
+
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL)
+} else {
+  var sequelize = new Sequelize("fyre_db", "root", process.env.MYSQL_PASS, {
+    host: "localhost",
+    port: 3306,
+    dialect: "mysql",
+    pool: {
+      max: 5,
+      min: 0,
+      idle: 10000
+    }
+  });
+}
 
 module.exports = sequelize;
 
